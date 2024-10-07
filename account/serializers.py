ae690c44850161cbf9de8cb8177edca155dc308f
Nikhil Bhagat <knewboy.nykhil@gmail.com>
2024-10-07 23:07:46 +0545

    added UserProfileView & UserChangePasswordView

--- account/serializers.py
+++ account/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from account.models import User
 
+# Serializers for user registration
 class UserRegisterSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
     class Meta:
@@ -21,8 +22,33 @@ class UserRegisterSerializer(serializers.ModelSerializer):
     def create(self, validate_data):
         return User.objects.create_user(**validate_data)
 
+# Serializers for user login
 class UserLoginSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(max_length=255)
     class Meta:
         model = User
         fields = ['email', 'password']
+
+# Serializers for user profile
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id','email', 'name']
+
+# Serializers for user change password
+class UserChangePasswordSerializer(serializers.Serializer):
+    password = serializers.CharField(max_length=255, style={'input_type':'password'}, write_only=True)
+    password2 = serializers.CharField(max_length=255, style={'input_type':'password'}, write_only=True)
+    class Meta:
+        fields = ['password', 'password2']
+
+    def validate(self, attrs):
+        password = attrs.get('password')
+        password2 = attrs.get('password2')
+        user = self.context.get('user')
+        if password != password2:
+            raise serializers.ValidationError("Password and Confirm Password doesn't match")
+        user.set_password(password)
+        user.save()
+        return attrs
+    
--- account/views.py
+++ account/views.py
@@ -1,23 +1,36 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from account.serializers import UserRegisterSerializer, UserLoginSerializer
+from account.serializers import UserRegisterSerializer, UserLoginSerializer, UserProfileSerializer, UserChangePasswordSerializer
 from django.contrib.auth import authenticate
 from account.renderers import UserRenderer
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+# Function to get tokens for user
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
+
+# User register view
 class UserRegisterView(APIView):
     renderer_classes = [UserRenderer]
     def post(self, request, format=None):
         serializer = UserRegisterSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
            user = serializer.save()
+           token = get_tokens_for_user(user)
            return Response({
+            "token": token,
             "status": "success",
-            "message": "User registered successfully"
-        }, status=status.HTTP_201_CREATED)
+            "message": "User registered successfully",
+            }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+# User login view
 class UserLoginView(APIView):
     renderer_classes = [UserRenderer]
     def post(self, request, format=None):
@@ -27,10 +40,29 @@ class UserLoginView(APIView):
             password = serializers.data.get('password')
             user = authenticate(email=email, password=password)
             if user is not None:
+                token = get_tokens_for_user(user)
                 return Response({
+                    "token": token,
                     "status": "success",
                     "message": "User login successfully"
-                }, status=status.HTTP_200_OK)
+                   }, status=status.HTTP_200_OK)
             else:
                 return Response({'errors' : {'none_field_error': ['Email or password is not valid']}}, status=status.HTTP_404_NOT_FOUND)
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# User profile view    
+class UserProfileView(APIView):
+    renderer_classes = [UserRenderer]
+    permission_classes = [IsAuthenticated]
+    def get(self, request, format=None):
+        serializer = UserProfileSerializer(request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+# User Change Password view
+class UserChangePasswordView(APIView):
+  renderer_classes = [UserRenderer]
+  permission_classes = [IsAuthenticated]
+  def post(self, request, format=None):
+    serializer = UserChangePasswordSerializer(data=request.data, context={'user':request.user})
+    serializer.is_valid(raise_exception=True)
+    return Response({'msg':'Password Changed Successfully'}, status=status.HTTP_200_OK)
